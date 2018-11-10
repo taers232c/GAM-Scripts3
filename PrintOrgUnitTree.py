3aac8f36b812b47dc66a745ba1e17a5a5a553523
Ross Scroggs <ross.scroggs@gmail.com>
2018-11-10 06:32:06 -0800

    Add additional capabilities

--- PrintOrgUnitTree.py
+++ PrintOrgUnitTree.py
@@ -4,7 +4,7 @@
 # Note: This script can use Basic or Advanced GAM:
 #	https://github.com/jay0lee/GAM
 #	https://github.com/taers232c/GAMADV-X, https://github.com/taers232c/GAMADV-XTD, https://github.com/taers232c/GAMADV-XTD3
-# Customize: Change QUOTE_CHAR, SHOW_LABELS, FIELD_DELIMITER, INDENT_SPACES, LINE_TERMINATOR as required/desired
+# Customize: Change QUOTE_CHAR, SHOW_EMPTY_OUS, SHOW_LABELS, FIELD_DELIMITER, INDENT_SPACES, LINE_TERMINATOR as required/desired
 # Usage:
 # 1: Get Org Units
 #  $ gam print ous > OrgUnits.csv
@@ -15,6 +15,10 @@
 #    Omit the third parameter or specify - to write to stdout
 #  $ python PrintOrgUnitTree.py ./OrgUnits.csv ./Users.csv ./OrgUnitTree.txt
 #  $ python PrintOrgUnitTree.py ./OrgUnits.csv ./CrOS.csv
+# 4: You can pipe data into the script, replace the second parameter with -
+#  $ gam print users fields primaryEmail,orgunitpath,name | python PrintOrgUnitTree.py ./OrgUnits.csv - ./OrgUnitTree.txt
+# 5: With Advanced GAM you can select subsets of Users/CrOS devices; this requires an additional API call per User/CrOS device to get the specified fields
+#  $ gam group students print users fields primaryEmail,orgunitpath,name | python PrintOrgUnitTree.py ./OrgUnits.csv - ./OrgUnitTree.txt
 """
 
 import csv
@@ -22,6 +26,7 @@ import sys
 
 QUOTE_CHAR = '"' # Adjust as needed to properly read CSV files
 
+SHOW_EMPTY_OUS = True # Should empty OUs be displayed
 SHOW_LABELS = True # Should field labels be displayed
 SELECTED_FIELDS = [] # Only display selected fields ['primaryEmail',] ['deviceId', 'notes']
 FIELD_DELIMITER = ', '# Delimiter between fields
@@ -47,33 +52,43 @@ for row in inputCSV:
   orgUnitsTree[row['orgUnitPath']] = []
 inputFile.close()
 
-inputFile = open(sys.argv[2], 'r', encoding='utf-8')
+if sys.argv[2] != '-':
+  inputFile = open(sys.argv[2], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
 inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
 inputFieldNames = inputCSV.fieldnames
+if inputFieldNames is None:
+  sys.stderr.write('Error: no headers in Org Units file {0}\n'.format(sys.argv[2]))
+  sys.exit(2)
 if SELECTED_FIELDS:
   fieldNames = []
   for field in SELECTED_FIELDS:
     if field not in inputFieldNames:
       sys.stderr.write('Error: selected field {0} is not in Data file {1} field names: {2}\n'.format(field, sys.argv[2], ','.join(inputFieldNames)))
-      sys.exit(2)
+      sys.exit(3)
     fieldNames.append(field)
 else:
   fieldNames = inputFieldNames[:]
   fieldNames.remove('orgUnitPath')
 if 'orgUnitPath' not in inputFieldNames:
   sys.stderr.write('Error: no header orgUnitPath in Data file {0} field names: {1}\n'.format(sys.argv[2], ','.join(inputFieldNames)))
-  sys.exit(3)
+  sys.exit(4)
 for row in inputCSV:
   if row['orgUnitPath'] is not None:
     orgUnitsTree[row['orgUnitPath']].append(row)
-inputFile.close()
+if inputFile != sys.stdin:
+  inputFile.close()
 
 for orgUnitPath in orgUnits:
-  outputFile.write('{0}\n'.format(orgUnitPath))
-  for child in orgUnitsTree[orgUnitPath]:
-    if SHOW_LABELS:
-      outputFile.write(INDENT_SPACES+FIELD_DELIMITER.join(['{0}: {1}'.format(field, child[field]) for field in fieldNames])+LINE_TERMINATOR)
-    else:
-      outputFile.write(INDENT_SPACES+FIELD_DELIMITER.join([child[field] for field in fieldNames])+LINE_TERMINATOR)
+  count = len(orgUnitsTree[orgUnitPath])
+  if SHOW_EMPTY_OUS or count > 0:
+    outputFile.write('{0}: {1}\n'.format(orgUnitPath, count))
+    if count > 0:
+      for child in orgUnitsTree[orgUnitPath]:
+        if SHOW_LABELS:
+          outputFile.write(INDENT_SPACES+FIELD_DELIMITER.join(['{0}: {1}'.format(field, child[field]) for field in fieldNames])+LINE_TERMINATOR)
+        else:
+          outputFile.write(INDENT_SPACES+FIELD_DELIMITER.join([child[field] for field in fieldNames])+LINE_TERMINATOR)
 if outputFile != sys.stdout:
   outputFile.close()
