4f9b75ecd1c8911ffa874c4402cff4c93e4c8a01
Ross Scroggs <ross.scroggs@gmail.com>
2019-03-01 08:42:58 -0800

    Include allowFileDiscovery option

--- GetUserNonOwnerDomainDriveACLs.py
+++ GetUserNonOwnerDomainDriveACLs.py
@@ -5,7 +5,7 @@
 # Note: This script can use Basic or Advanced GAM:
 #	https://github.com/jay0lee/GAM
 #	https://github.com/taers232c/GAMADV-X, https://github.com/taers232c/GAMADV-XTD, https://github.com/taers232c/GAMADV-XTD3
-# Customize: Set FILE_NAME and ALT_FILE_NAME based on your environment. Set DOMAIN_LIST.
+# Customize: Set FILE_NAME and ALT_FILE_NAME based on your environment. Set DOMAIN_LIST and DESIRED_ALLOWFILEDISCOVERY.
 # 1: Use print filelist to get selected ACLs
 #    gam user testuser@domain.com print filelist id title permissions > filelistperms.csv
 # 2: From that list of ACLs, output a CSV file with headers "Owner,driveFileId,driveFileTitle,permissionId,role,type,emailAddress,domain"
@@ -27,8 +27,10 @@ ALT_FILE_NAME = 'name'
 #FILE_NAME = 'name'
 #ALT_FILE_NAME = 'title'
 
-# Substitute your domain(s) in the list below, e.g., DOMAIN_LIST = ['domain.com',] DOMAIN_LIST = ['domain1.com', 'domain2.com',]
-DOMAIN_LIST = ['domain.com',]
+# If you want to limit finding ACLS for a specific list of domains, use the list below, e.g., DOMAIN_LIST = ['domain.com',] DOMAIN_LIST = ['domain1.com', 'domain2.com',]
+DOMAIN_LIST = []
+# Specify desired value of allowFileDiscovery field: True, False, Any (matches True and False)
+DESIRED_ALLOWFILEDISCOVERY = 'Any'
 
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
@@ -53,6 +55,9 @@ for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
     if mg and v:
       permissions_N = mg.group(1)
       if v == u'domain':
+        allowFileDiscovery = row.get('permissions.{0}.allowFileDiscovery'.format(permissions_N), str(row.get('permissions.{0}.withLink'.format(permissions_N)) == 'False'))
+        if DESIRED_ALLOWFILEDISCOVERY not in ('Any', allowFileDiscovery):
+          continue
         domain = row['permissions.{0}.domain'.format(permissions_N)]
         emailAddress = ''
       elif v in ['user', 'group']:
@@ -62,7 +67,7 @@ for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
         domain = emailAddress[emailAddress.find(u'@')+1:]
       else:
         continue
-      if (domain in DOMAIN_LIST) and (v != 'user' or row['permissions.{0}.role'.format(permissions_N)] != 'owner' or emailAddress != row['Owner']):
+      if (not DOMAIN_LIST or domain in DOMAIN_LIST) and (v != 'user' or row['permissions.{0}.role'.format(permissions_N)] != 'owner' or emailAddress != row['Owner']):
         outputCSV.writerow({'Owner': row['Owner'],
                             'driveFileId': row['id'],
                             'driveFileTitle': row.get(FILE_NAME, row.get(ALT_FILE_NAME, 'Unknown')),
