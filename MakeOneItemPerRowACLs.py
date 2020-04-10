ec542ceb2a31bd94fee002783b6fbf433c0b7134
Ross Scroggs <ross.scroggs@gmail.com>
2020-04-10 14:24:26 -0700

    Add full filtering capabilities

--- MakeOneItemPerRowACLs.py
+++ MakeOneItemPerRowACLs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-# Purpose: Convert output from print filelist to put one ACL per row
+# Purpose: Convert output from print filelist to put one ACL per row; you can filter for specific ACLs.
 # Note: This script can use Basic or Advanced GAM:
 #	https://github.com/jay0lee/GAM
 #	https://github.com/taers232c/GAMADV-XTD3
+# Customize: Set USER_LIST, GROUP_LIST. DOMAIN_LIST, ROLE_LIST, TYPE_LIST, DESIRED_ALLOWFILEDISCOVERY,
+#	DROP_GENERAL_COLUMNS, DROP_PERMISSION_COLUMNS.
 # Usage:
 # 1: Get ACLs for all files, replace user@domain.com with your user selection in the command below
-#    For enhanced file selection in Advanced gam see: https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Drive-Files#display-file-lists
+#    For enhanced file selection in Advanced Gam see: https://github.com/taers232c/GAMADV-XTD3/wiki/Users-Drive-Files#display-file-lists
 #  $ Basic: gam user user@domain.com print filelist id title permissions > filelistperms.csv
 #  $ Advanced: gam redirect csv ./filelistperms.csv user user@domain.com print filelist fields id,name,permissions
 # 2: From that list of files, output a CSV file that lists one ACL per row
@@ -16,6 +18,39 @@
 import csv
 import re
 import sys
+
+# Specify specific user(s), e.g., USER_LIST = ['user1@domain.com'] USER_LIST = ['user1@domain.com', 'user2@domain.com']
+# The list should be empty if you're only specifiying domains in DOMAIN_LIST, e.g. USER_LIST = []
+USER_LIST = []
+
+# Specify specific group(s), e.g., GROUP_LIST = ['group1@domain.com'] GROUP_LIST = ['group1@domain.com', 'group2@domain.com']
+# The list should be empty if you're only specifiying domains in DOMAIN_LIST, e.g. GROUP_LIST = []
+GROUP_LIST = []
+
+# Specify specific domain(s) if you want all groups/users in the domain, e.g., DOMAIN_LIST = ['domain.com'] DOMAIN_LIST = ['domain1.com', 'domain2.com']
+# The list should be empty if you're only specifiying groups in GROUP_LIST or users in USER_LIST, e.g. DOMAIN__LIST = []
+DOMAIN_LIST = []
+
+# Specify specific permission role value(s) ('owner', 'organizer', 'fileOrganizer', 'writer', 'commenter', 'reader'), e.g., ROLE_LIST = ['writer', 'commenter']
+# The list should be empty if you want all roles, e.g, ROLE_LIST = []
+ROLE_LIST = []
+
+# Specify specific permission type value(s) ('user', 'group, 'domain', 'anyone'), e.g., TYPE_LIST = ['user', 'group']
+# The list should be empty if you want all types, e.g, TYPE_LIST = []
+TYPE_LIST = []
+
+# Specify desired value of allowFileDiscovery field: True, False, Any (matches True and False)
+# allowFileDiscovery False = withLink True
+# allowFileDiscovery True = withLink False
+DESIRED_ALLOWFILEDISCOVERY = 'Any'
+
+# Specify generalcolumns you don't want in the output. e.g., photoLink.
+# The list should be empty if you want all general columns, e.g, DROP_PERMISSION_COLUMNS = []
+DROP_GENERAL_COLUMNS = ['permissions']
+
+# Specify permission columns you don't want in the output. e.g., photoLink.
+# The list should be empty if you want all permission columns, e.g, DROP_PERMISSION_COLUMNS = []
+DROP_PERMISSION_COLUMNS = ['photoLink']
 
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
@@ -38,9 +73,10 @@ inputFieldnames = []
 for fieldname in inputCSV.fieldnames:
   mg = PERMISSIONS_N_FIELD.match(fieldname)
   if mg:
-    if mg.group(2) not in permissionFields:
-      permissionFields.add(mg.group(2))
-      inputFieldnames.append(f'permission.{mg.group(2)}')
+    field = mg.group(2)
+    if (not DROP_PERMISSION_COLUMNS or field not in DROP_PERMISSION_COLUMNS) and field not in permissionFields:
+      permissionFields.add(field)
+      inputFieldnames.append(f'permission.{field}')
   else:
     inputFieldnames.append(fieldname)
 
@@ -60,8 +96,43 @@ for row in inputCSV:
       baseRow[k] = v
   for k, v in iter(permissions.items()):
     newRow = baseRow.copy()
+    if ROLE_LIST and v['role'] not in ROLE_LIST:
+      continue
+    vtype = v['type']
+    if not vtype:
+      continue
+    if TYPE_LIST and vtype not in TYPE_LIST:
+      continue
+    if vtype == 'user':
+      emailAddress = v['emailAddress']
+      domain = v['domain']
+      if DOMAIN_LIST and domain not in DOMAIN_LIST:
+        continue
+      if USER_LIST and emailAddress not in USER_LIST:
+        continue
+    elif vtype == 'group':
+      emailAddress = v['emailAddress']
+      domain = v['domain']
+      if DOMAIN_LIST and domain not in DOMAIN_LIST:
+        continue
+      if GROUP_LIST and emailAddress not in GROUP_LIST:
+        continue
+    elif vtype == 'domain':
+      domain = v['domain']
+      if DOMAIN_LIST and domain not in DOMAIN_LIST:
+        continue
+      if DESIRED_ALLOWFILEDISCOVERY != 'Any':
+        allowFileDiscovery = v.get('allowFileDiscovery', str(row.get('withLink', '') == 'False'))
+        if DESIRED_ALLOWFILEDISCOVERY != allowFileDiscovery:
+          continue
+    else: # vtype == 'anyone'
+      if DESIRED_ALLOWFILEDISCOVERY != 'Any':
+        allowFileDiscovery = v.get('allowFileDiscovery', str(row.get('withLink', '') == 'False'))
+        if DESIRED_ALLOWFILEDISCOVERY != allowFileDiscovery:
+          continue
     for kp, kv in sorted(v.items()):
-      newRow[f'permission.{kp}'] = kv
+      if not DROP_PERMISSION_COLUMNS or kp not in DROP_PERMISSION_COLUMNS:
+        newRow[f'permission.{kp}'] = kv
     outputCSV.writerow(newRow)
 
 if inputFile != sys.stdin:
