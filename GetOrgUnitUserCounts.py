8351438cd96a1051236d285e96a639c963060a3b
Ross Scroggs <ross.scroggs@gmail.com>
2020-06-27 16:48:59 -0700

    Update to show empty Org Units

--- GetOrgUnitUserCounts.py
+++ GetOrgUnitUserCounts.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """
-# Purpose: Show the number of users in each Org unit
+# Purpose: Show the number of Users in each Org Unit
 # Note: This script can use Basic or Advanced GAM:
 #	https://github.com/jay0lee/GAM
 #	https://github.com/taers232c/GAMADV-XTD3
 # Customize: Set SHOW_SUSPENDED, SHOW_SUSPENSION_REASON and SHOW_TOTALS
 # Usage:
-# 1: Get users; omit suspended if you don't want suspension info
-#  $ gam print users ou suspended > users.csv
-# 2: From that list of users, output a CSV file with user counts for each Org Unit
-#  $ python GetOrgUnitUserCounts.py users.csv.csv orgunitcounts.csv
+# 1: Get OrgUnits
+#  $ gam print ous > OrgUnits.csv
+# 2: Get Users; omit suspended if you don't want suspension info
+#  $ gam print users ou suspended > Users.csv
+# 3: From those lists of Users and Org Units, output a CSV file with user counts for each Org Unit
+#  $ python GetOrgUnitUserCounts.py ./OrgUnits.csv ./Users.csv ./OrgUnitUserCounts.csv
 """
 
 import csv
-import re
 import sys
 
 SHOW_SUSPENDED = True # False if you don't want suspension info
@@ -23,27 +24,38 @@ SHOW_TOTALS = True # False if you don't want totals
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
 
-PERMISSIONS_N_TYPE = re.compile(r"permissions.(\d+).type")
+orgUnits = {}
+inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
+inputFieldNames = inputCSV.fieldnames
+if 'orgUnitPath' not in inputFieldNames:
+  sys.stderr.write('Error: no header orgUnitPath in Org Units file {0} field names: {1}\n'.format(sys.argv[1], ','.join(inputFieldNames)))
+  sys.exit(1)
+for row in inputCSV:
+  orgUnits[row['orgUnitPath']] = {'total' : 0, 'suspended': 0, 'suspensionReason': {}}
+inputFile.close()
 
-if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+if sys.argv[2] != '-':
+  inputFile = open(sys.argv[2], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
+inputFieldNames = inputCSV.fieldnames
+if 'orgUnitPath' not in inputFieldNames:
+  sys.stderr.write('Error: no header orgUnitPath in Users file {0} field names: {1}\n'.format(sys.argv[2], ','.join(inputFieldNames)))
+  sys.exit(1)
+fieldnames = ['orgUnitPath', 'total']
+checkSuspended = SHOW_SUSPENDED and 'suspended' in inputFieldNames
+if checkSuspended:
+  fieldnames.append('suspended')
+checkSuspensionReason = SHOW_SUSPENSION_REASON and 'suspensionReason' in inputFieldNames
+suspensionReasons = set()
+
+if (len(sys.argv) > 3) and (sys.argv[2] != '-'):
   outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
 else:
   outputFile = sys.stdout
 
-if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
-  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
-else:
-  inputFile = sys.stdin
-
-inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
-fieldnames = ['orgUnitPath', 'total']
-checkSuspended = SHOW_SUSPENDED and 'suspended' in inputCSV.fieldnames
-if checkSuspended:
-  fieldnames.append('suspended')
-checkSuspensionReason = SHOW_SUSPENSION_REASON and 'suspensionReason' in inputCSV.fieldnames
-suspensionReasons = set()
-
-orgUnits = {}
 totals = {'total' : 0, 'suspended': 0, 'suspensionReason': {}}
 for row in inputCSV:
   orgUnitPath = row['orgUnitPath']
