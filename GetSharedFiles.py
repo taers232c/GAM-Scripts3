81bbf78fd8a17775d8bbc4fad774f9bf9bd1f3bc
Ross Scroggs <ross.scroggs@gmail.com>
2020-07-12 16:02:11 -0700

    Add pm to reduce number of files processed

--- GetSharedFiles.py
+++ GetSharedFiles.py
@@ -8,8 +8,8 @@
 # 1: Get ACLs for all files, if you don't want all users, replace all users with your user selection in the command below
 #  $ Basic: gam all users print filelist id title permissions > filelistperms.csv
 #  $ Basic: gam user user@domain.com print filelist id title permissions > filelistperms.csv
-#  $ Advanced: gam config auto_batch_min 1 redirect csv ./filelistperms.csv multiprocess all users print filelist fields id,title,permissions
-#  $ Advanced: gam redirect csv ./filelistperms.csv user user@domain.com print filelist fields id,title,permissions
+#  $ Advanced: gam config auto_batch_min 1 redirect csv ./filelistperms.csv multiprocess all users print filelist fields id,title,permissions pm not role owner em
+#  $ Advanced: gam redirect csv ./filelistperms.csv user user@domain.com print filelist fields id,title,permissions pm not role owner em
 # 2: From that list of ACLs, output a CSV file that lists only the shared files.
 #  $ python GetSharedFiles.py filelistperms.csv sharedfiles.csv
 """
