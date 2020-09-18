5990954da7396c5a691f1866b30ccded41734d25
Ross Scroggs <ross.scroggs@gmail.com>
2020-09-18 10:51:40 -0700

    Create ShowGroupMemberTree.py

---
+++ ShowGroupMemberTree.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+# Purpose: Produce a file to show hierarchial group membership
+# Note: This requires Advanced GAM:
+#	https://github.com/taers232c/GAMADV-XTD3
+# Customize: SHOW_ROLE
+# Usage:
+# 1: Get group members
+#  $ gam redirect csv ./GroupMembers.csv print group-members fields email,role recursive noduplicates
+# 2: From that list of group members, output a file showing hierarchial group membership
+#  $ python3 ShowGroupMemberTree.py ./GroupMembers.csv ./GroupMemberTree.txt
+"""
+
+import csv
+import sys
+
+QUOTE_CHAR = '"' # Adjust as needed
+
+SHOW_ROLE = False # True - display role, False - don't display role
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+
+if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
+  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
+
+Groups = {}
+for row in inputCSV:
+  group = row['group']
+  Groups.setdefault(group, {})
+  Groups[group][row['email']] = row['role']
+for group, members in sorted(iter(Groups.items())):
+  print(f'Group: {group}')
+  for member, role in sorted(iter(members.items())):
+    if SHOW_ROLE:
+      print(f"  {member}:{role}")
+    else:
+      print(f"  {member}")
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
