2ac8f7f5f0336b7e1d5597c50ff4331240463124
Ross Scroggs <ross.scroggs@gmail.com>
2020-12-06 09:57:04 -0800

    Add complex Country Code to OU mapping

--- GetUserCCOrgs.py
+++ GetUserCCOrgs.py
@@ -3,7 +3,7 @@
 # Purpose: Move root users to an Org Unit based on their work address countryCode
 # Note: This script requires Advanced GAM:
 #	https://github.com/taers232c/GAMADV-XTD3
-# Customize: Set ORG_UNIT_MAP
+# Customize: Set ORG_UNIT_FORMAT_MAP or ORG_UNIT_DICT_MAP
 # Python: Use python or python3 below as appropriate to your system; verify that you have version 3
 #  $ python -V   or   python3 -V
 #  Python 3.x.y
@@ -22,8 +22,14 @@ import csv
 import re
 import sys
 
-# Change format as desired, {0} is replaced by countryCode
-ORG_UNIT_MAP = '{0}'
+# If you simply want to substitute the country code into a string that defines the OU,
+# use ORG_UNIT_FORMAT_MAP, Change the format as desired, {0} is replaced by countryCode
+# For example, ORG_UNIT_FORMAT_MAP = '/SalesByCountry/{0}'
+ORG_UNIT_FORMAT_MAP = '{0}'
+
+# If you want to map the country codes to OUs in a more cmplex manner, use ORG_UNIT_DICT_MAP.
+# For example, ORG_UNIT_DICT_MAP = {'US': '/SalesByCountry/UnitedStates', 'CA': '/SalesByCountry/Canada'}
+ORG_UNIT_DICT_MAP = {}
 
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
@@ -48,10 +54,17 @@ for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
       mg = ADDRESSES_N_TYPE.match(k)
       if mg and v == 'work':
         addresses_N = mg.group(1)
-        org = ORG_UNIT_MAP.format(row[f'addresses.{addresses_N}.countryCode'])
-        if org:
-          outputCSV.writerow({'Org': org,
-                              'primaryEmail': row['primaryEmail']})
+        cc = row[f'addresses.{addresses_N}.countryCode']
+        if cc:
+          if ORG_UNIT_DICT_MAP:
+            org = ORG_UNIT_DICT_MAP.get(cc, None)
+            if org:
+              outputCSV.writerow({'Org': org, 'primaryEmail': row['primaryEmail']})
+              break
+            sys.stderr.write(f'ERROR: No OU for country code {cc} for user {row["primaryEmail"]}\n')
+          else:
+            outputCSV.writerow({'Org': ORG_UNIT_FORMAT_MAP.format(cc), 'primaryEmail': row['primaryEmail']})
+            break
 
 if inputFile != sys.stdin:
   inputFile.close()
