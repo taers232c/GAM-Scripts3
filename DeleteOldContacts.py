eae804e8b0a8b065179db84dc6a921f1b78f1951
Ross Scroggs <ross.scroggs@gmail.com>
2020-12-29 10:21:12 -0800

    Create DeleteOldContacts.py

---
+++ DeleteOldContacts.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+# Purpose: Delete old contacts from user's personal contacts
+# Note: This script requires Advanced GAM:
+#	https://github.com/taers232c/GAMADV-XTD3
+# Python: Use python or python3 below as appropriate to your system; verify that you have version 3
+#  $ python -V   or  python3 -V
+#  Python 3.x.y
+# Usage:
+# 1: Generate OldContacts.csv, header row Email, one email address per row
+# 2: Get current contactsfor all users, if you don't want all users, replace all users with your user selection in the command below
+#  $ gam redirect csv ./CurrentContacts.csv all users print contacts fields email,name
+# 3: From that list of user's contacts, output a CSV file with headers: User,ContactID,Name,Email
+#    that shows user's contacts with an email address from OldContacts.csv
+#  $ python3 DeleteOldContacts.py ./OldContacts.csv ./CurrentContacts.csv ./DeleteContacts.csv
+# 4: Inspect DeleteContacts.csv, verify that it makes sense and then proceed
+# 5: Delete the contacts
+#  $ gam csv DeleteContacts.csv gam user "~User" delete contact "~ContactID"
+"""
+
+import csv
+import re
+import sys
+
+QUOTE_CHAR = '"' # Adjust as needed
+LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
+
+EMAILS_N_ADDRESS = re.compile(r"Emails.(\d+).address")
+
+OldContacts = set()
+
+inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  OldContacts.add(row['Email'].lower())
+inputFile.close()
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  inputFile = open(sys.argv[2], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+
+if (len(sys.argv) > 3) and (sys.argv[3] != '-'):
+  outputFile = open(sys.argv[3], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+outputCSV = csv.DictWriter(outputFile, ['User','ContactID','Name','Email'], lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
+outputCSV.writeheader()
+
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  for k, v in iter(row.items()):
+    mg = EMAILS_N_ADDRESS.match(k)
+    if mg and v.lower() in OldContacts:
+      outputCSV.writerow({'User': row['User'],
+                          'ContactID': row['ContactID'],
+                          'Name': row['Name'],
+                          'Email': v})
+
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
