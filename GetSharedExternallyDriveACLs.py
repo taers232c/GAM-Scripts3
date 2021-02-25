f9cf6fa5dd0063ee7bbd28ff1c557f74327e02a1
Ross Scroggs <ross.scroggs@gmail.com>
2021-02-25 10:03:18 -0800

    Create GetSharedExternallyDriveACLs.py

---
+++ GetSharedExternallyDriveACLs.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+# Purpose: For a Google Drive User(s), show all drive file ACLs for files shared outside of a list of specified domains, this includes anyone
+# Note: This script can use Basic or Advanced GAM:
+#	https://github.com/jay0lee/GAM
+#	https://github.com/taers232c/GAMADV-XTD3
+# Customize: Set DOMAIN_LIST.
+# Python: Use python or python3 below as appropriate to your system; verify that you have version 3
+#  $ python -V   or   python3 -V
+#  Python 3.x.y
+# Usage:
+# 1: Get ACLs for all files, if you don't want all users, replace all users with your user selection in the command below
+#    If you are looking for ACLs referencing specific domains, list them in DOMAIN_LIST.
+#    For Advanced GAM, add the following clause to the command for each domain in DOMAIN_LIST: pm not domain xyz.com em
+#  $ Basic GAM: gam all users print filelist id title permissions owners > filelistperms.csv
+#  $ Advanced GAM: gam config auto_batch_min 1 redirect csv ./filelistperms.csv multiprocess all users print filelist fields id,title,permissions,owners.emailaddress pm type anyone em pm not domain xyz.com em
+# 2: From that list of ACLs, output a CSV file with headers "Owner,driveFileId,driveFileTitle,permissionId,role,domain,allowFileDiscovery"
+#    that lists the driveFileIds and permissionIds for all ACLs shared with the selected domains.
+#    (n.b., driveFileTitle, role, domain and allowFileDiscovery are not used in the next step, they are included for documentation purposes)
+#  $ python3 GetSharedExternallyDriveACLs.py filelistperms.csv deleteperms.csv
+# 3: Inspect deleteperms.csv, verify that it makes sense and then proceed
+# 4: Delete the ACLs
+#  $ gam csv deleteperms.csv gam user "~Owner" delete drivefileacl "~driveFileId" "~permissionId"
+"""
+
+import csv
+import re
+import sys
+
+FILE_NAME = 'name'
+ALT_FILE_NAME = 'title'
+
+# Substitute your domain(s) in the list below, e.g., DOMAIN_LIST = ['domain.com',] DOMAIN_LIST = ['domain1.com', 'domain2.com',]
+DOMAIN_LIST = ['domain.com']
+
+QUOTE_CHAR = '"' # Adjust as needed
+LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
+
+PERMISSIONS_N_TYPE = re.compile(r"permissions.(\d+).type")
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+outputCSV = csv.DictWriter(outputFile, ['Owner', 'driveFileId', 'driveFileTitle',
+                                        'permissionId', 'role', 'type', 'emailAddress', 'domain'],
+                           lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
+outputCSV.writeheader()
+
+if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
+  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  for k, v in iter(row.items()):
+    mg = PERMISSIONS_N_TYPE.match(k)
+    if mg and v:
+      permissions_N = mg.group(1)
+      if v == 'domain':
+        emailAddress = ''
+        domain = row[f'permissions.{permissions_N}.domain']
+      elif v in ['user', 'group']:
+        if row.get(f'permissions.{permissions_N}.deleted') == 'True':
+          continue
+        emailAddress = row[f'permissions.{permissions_N}.emailAddress']
+        domain = emailAddress[emailAddress.find('@')+1:]
+      else: #anyone
+        emailAddress = ''
+        domain = ''
+      if v == 'anyone' or domain not in DOMAIN_LIST:
+        outputCSV.writerow({'Owner': row['Owner'],
+                            'driveFileId': row['id'],
+                            'driveFileTitle': row.get(FILE_NAME, row.get(ALT_FILE_NAME, 'Unknown')),
+                            'permissionId': f'id:{row[f"permissions.{permissions_N}.id"]}',
+                            'role': row[f'permissions.{permissions_N}.role'],
+                            'type': v,
+                            'emailAddress': emailAddress,
+                            'domain': domain})
+
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
