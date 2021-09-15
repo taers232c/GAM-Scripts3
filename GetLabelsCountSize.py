fcb6a76e9cc1a492ef6844f9a7496e1a8d3bcb68
Ross Scroggs <ross.scroggs@gmail.com>
2021-09-15 12:16:55 -0700

    Create GetLabelsCountSize.py

---
+++ GetLabelsCountSize.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+# Purpose: Create a CSV file that totals message label data: count and size
+# Note: This script requires Advanced GAM:
+#	https://github.com/taers232c/GAMADV-XTD3
+# Python: Use python or python3 below as appropriate to your system; verify that you have version 3
+#  $ python -V   or   python3 -V
+#  Python 3.x.y
+# Usage:
+# 1: Get the label data
+#  Single user
+#  $ gam redirect csv ./LabelData.csv user user@domain.com print messages showlabels showsize headers "" delimiter '|'
+#  Multiple users; replace all users as desired
+#  $ gam config auto_batch_min 1 redirect csv ./LabelData.csv multiprocess all users print messages showlabels showsize headers "" delimiter '|'
+# 2: python3 GetLabelsCountSize.py LabelData.csv LabelSummary.csv
+"""
+
+import csv
+import sys
+
+QUOTE_CHAR = '"' # Adjust as needed
+LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
+DELIMITER = '|'
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+outputCSV = csv.DictWriter(outputFile, ['User', 'Label', 'Count', 'SizeEstimate'], lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
+outputCSV.writeheader()
+
+if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
+  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+
+Users = {}
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  user = row['User']
+  Users.setdefault(user, {})
+  size = int(row['SizeEstimate'])
+  for label in row['Labels'].split(DELIMITER):
+    Users[user].setdefault(label, {'Count': 0, 'SizeEstimate': 0})
+    Users[user][label]['Count'] +=1
+    Users[user][label]['SizeEstimate'] += size
+
+for user in sorted(Users):
+  for label, data in sorted(iter(Users[user].items())):
+    outputCSV.writerow({'User': user,
+                        'Label': label,
+                        'Count': data['Count'],
+                        'SizeEstimate': data['SizeEstimate']})
+
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
