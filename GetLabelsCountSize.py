115e1ae811b72570c674a0b0284494f57660ad79
Ross Scroggs <ross.scroggs@gmail.com>
2021-09-15 12:36:37 -0700

    Update GetLabelsCountSize.py

--- GetLabelsCountSize.py
+++ GetLabelsCountSize.py
@@ -3,6 +3,7 @@
 # Purpose: Create a CSV file that totals message label data: count and size
 # Note: This script requires Advanced GAM:
 #	https://github.com/taers232c/GAMADV-XTD3
+# Customize: DELIMITER, SHOW_TOTALS
 # Python: Use python or python3 below as appropriate to your system; verify that you have version 3
 #  $ python -V   or   python3 -V
 #  Python 3.x.y
@@ -20,7 +21,9 @@ import sys
 
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
-DELIMITER = '|'
+
+DELIMITER = '|' # Must match delimiter from command line
+SHOW_TOTALS = False # False: Don't show total label counts/size for each user; True: Do show
 
 if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
   outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
@@ -45,11 +48,20 @@ for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
     Users[user][label]['SizeEstimate'] += size
 
 for user in sorted(Users):
+  count = 0
+  size = 0
   for label, data in sorted(iter(Users[user].items())):
+    count += data['Count']
+    size += data['SizeEstimate']
     outputCSV.writerow({'User': user,
                         'Label': label,
                         'Count': data['Count'],
                         'SizeEstimate': data['SizeEstimate']})
+  if SHOW_TOTALS:
+    outputCSV.writerow({'User': user,
+                        'Label': 'Total',
+                        'Count': count,
+                        'SizeEstimate': size})
 
 if inputFile != sys.stdin:
   inputFile.close()
