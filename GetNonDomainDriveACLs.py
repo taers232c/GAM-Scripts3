8469bba241b96b80a28036f7aec4eeb74164aad6
Ross Scroggs <ross.scroggs@gmail.com>
2021-12-15 10:06:07 -0800

    Add DOMAIN_EXPRESSIONS

--- GetNonDomainDriveACLs.py
+++ GetNonDomainDriveACLs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
 # Purpose: For a Google Drive User(s), show all drive file ACLs for files shared outside of a list of specified domains
-#          You specify a list of domains, DOMAIN_LIST, and indicate whether this list is exclusive/inclusive
-#          EXCLUSIVE_DOMAINS = True: exclude domains in DOMAIN_LIST from the output
-#          EXCLUSIVE_DOMAINS = False: include domains in DOMAIN_LIST in the output
+#          You specify a list of domains, DOMAIN_LIST, or a list of domain expressions, DOMAIN_EXPRESSIONS
+#	   Indicate whether these lists are exclusive/inclusive
+#          EXCLUSIVE_DOMAINS = True: exclude domains in DOMAIN_LIST/DOMAIN_EXPRESSIONS from the output
+#          EXCLUSIVE_DOMAINS = False: include domains in DOMAIN_LIST/DOMAIN_EXPRESSIONS in the output
 #          You can include/exclude shares to anyone in the ouput
 #          INCLUDE_ANYONE = True: include shares to anyone in the output
 #          INCLUDE_ANYONE = False: exclude shares to anyone from the output
 # Note: This script can use Basic or Advanced GAM:
 #	https://github.com/jay0lee/GAM
 #	https://github.com/taers232c/GAMADV-XTD3
-# Customize: Set DOMAIN_LIST, EXCLUSIVE_DOMAINS, INCLUDE_ANYONE
+# Customize: Set DOMAIN_LIST, DOMAIN_EXPRESSIONS, EXCLUSIVE_DOMAINS, INCLUDE_ANYONE
 # Python: Use python or python3 below as appropriate to your system; verify that you have version 3
 #  $ python -V   or   python3 -V
 #  Python 3.x.y
@@ -41,11 +42,17 @@ import sys
 FILE_NAME = 'name'
 ALT_FILE_NAME = 'title'
 
-# Substitute your domain(s) in the list below, e.g., DOMAIN_LIST = ['domain.com',] DOMAIN_LIST = ['domain1.com', 'domain2.com',]
-DOMAIN_LIST = ['domain.com']
+# Define your domain(s) in the list below,
+# e.g., DOMAIN_LIST = ['domain.com'] DOMAIN_LIST = ['domain1.com', 'domain2.com']
+DOMAIN_LIST = []
+
+# Provide a list of regular expressions that define your domains(s),
+# e.g., DOMAIN_EXPRESSIONS = [re.compile(r'@domain\.com$')] DOMAIN_EXPRESSIONS = [re.compile(r'@.*domain1\.com$'0, re.compile(r'@.*domain2\.com$')]
+DOMAIN_EXPRESSIONS = []
+
 # Indicate whether the list is exclusive or inclusive
-# EXCLUSIVE_DOMAINS = True: You're interested only in domains not in DOMAIN_LIST which would typically be your internal domains
-# EXCLUSIVE_DOMAINS = False: You're interested only in domains in DOMAIN_LIST which would typically be external domains
+# EXCLUSIVE_DOMAINS = True: You're interested only in domains not in DOMAIN_LIST/DOMAIN_EXPRESSIONS which would typically be your internal domains
+# EXCLUSIVE_DOMAINS = False: You're interested only in domains in DOMAIN_LIST/DOMAIN_EXPRESSIONS which would typically be external domains
 EXCLUSIVE_DOMAINS = True
 # Indicate whether shares to anyone should be included
 INCLUDE_ANYONE = True
@@ -54,6 +61,23 @@ QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
 
 PERMISSIONS_N_TYPE = re.compile(r"permissions.(\d+).type")
+
+def checkDomain(d):
+  if EXCLUSIVE_DOMAINS:
+    if DOMAIN_LIST and d in DOMAIN_LIST:
+      return False
+    for regex in DOMAIN_EXPRESSIONS:
+      if regex.search(d):
+        return False
+  else:
+    if DOMAIN_LIST and d not in DOMAIN_LIST:
+      return False
+    if DOMAIN_EXPRESSIONS:
+      for regex in DOMAIN_EXPRESSIONS:
+        if regex.search(d):
+          return True
+      return False
+  return not EXCLUSIVE_DOMAINS
 
 if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
   outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
@@ -88,8 +112,7 @@ for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
         emailAddress = ''
         domain = ''
       if ((v == 'anyone') or # Can only be true is INCLUDE_ANYONE = True
-          (EXCLUSIVE_DOMAINS and domain not in DOMAIN_LIST) or
-          (not EXCLUSIVE_DOMAINS and domain in DOMAIN_LIST)):
+          checkDomain(domain)):
         outputCSV.writerow({'Owner': row['owners.0.emailAddress'],
                             'driveFileId': row['id'],
                             'driveFileTitle': row.get(FILE_NAME, row.get(ALT_FILE_NAME, 'Unknown')),
