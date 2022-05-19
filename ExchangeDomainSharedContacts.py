5a0c0e525c11729e63d996dda0cb945d24feac3f
Kim Nilsson <github@no-substitute.com>
2022-05-19 15:50:54 +0200

    Update ExchangeDomainSharedContacts.py
    Second round of actions referenced the wrong domain for select.
    the first round was done with "select DomainA", which means the second round must be with "select DomainB".

--- ExchangeDomainSharedContacts.py
+++ ExchangeDomainSharedContacts.py
@@ -24,9 +24,9 @@ $ 3: Perform updates for DomainA domain shared contacts
 # 2: Find add, deletes, updates for DomainB domain shared contacts
 #  $ python3 ExchangeDomainSharedContacts.py DomainA.com ./DomainAUsers.csv ./DomainBContacts.csv ./DomainBContactUpdates.csv
 $ 3: Perform updates for DomainB domain shared contacts
-#  $ gam select DomainA redirect stdout ./DomainBContactDeletes.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Delete gam delete contact "~ContactID"
-#  $ gam select DomainA redirect stdout ./DomainBContactUpdates.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Update gam update contact "~ContactID" givenName "~givenName" familyname "~familyName" name "~fullName"
-#  $ gam select DomainA redirect stdout ./DomainBContactAdds.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Add gam create contact email work '~ContactID' primary givenName "~givenName" familyname "~familyName" name "~fullName"
+#  $ gam select DomainB redirect stdout ./DomainBContactDeletes.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Delete gam delete contact "~ContactID"
+#  $ gam select DomainB redirect stdout ./DomainBContactUpdates.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Update gam update contact "~ContactID" givenName "~givenName" familyname "~familyName" name "~fullName"
+#  $ gam select DomainB redirect stdout ./DomainBContactAdds.txt redirect stderr stdout loop ./DomainBContactUpdates.csv matchfield Action Add gam create contact email work '~ContactID' primary givenName "~givenName" familyname "~familyName" name "~fullName"
 """
 
 import csv
