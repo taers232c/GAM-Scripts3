272b44911db740520ade9500f80838d0a846fe90
Ross Scroggs <ross.scroggs@gmail.com>
2022-09-11 15:23:32 -0700

    Create GetAllowFileDiscoveryDriveACLs.py

---
+++ GetAllowFileDiscoveryDriveACLs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+# Purpose: For a Google Drive User(s), get all drive file ACLs for files shared with anyone/domain allowfilediscovery
+# Note: This script can use Basic or Advanced GAM:
+#	https://github.com/GAM-team/GAM - Version 6.07+
+#	https://github.com/taers232c/GAMADV-XTD3 - Version 6.04.17+
+# Python: Use python or python3 below as appropriate to your system; verify that you have version 3
+#  $ python -V   or   python3 -V
+#  Python 3.x.y
+# Usage:
+# 1: Get ACLs for all files, if you don't want all users, replace all users with your user selection in the command below
+#  $ Basic: gam all users print filelist id title permissions owners query "(visibility='anyoneCanFind' or visibility='domainCanFind')" > filelistperms.csv
+#  $ Advanced: gam config auto_batch_min 1 redirect csv ./filelistperms.csv multiprocess all users print filelist fields id,name,permissions,owners.emailaddress query "(visibility='anyoneCanFind' or visibility='domainCanFind')"
+# 2: From that list of ACLs, output a CSV file with headers "Owner,driveFileId,driveFileTitle,permissionId,role,type,domain,allowFileDiscovery"
+#    that lists the driveFileIds, permissionIds and details for all ACLs shared with anyone/domain allowFileDiscovery
+#  $ python3 GetAllowFileDiscoveryDriveACLs.py filelistperms.csv deleteperms.csv
+# 3: Inspect deleteperms.csv, verify that it makes sense and then proceed
+# 4: If desired, delete the ACLs
+#  $ gam csv ./deleteperms.csv gam user "~Owner" delete drivefileacl "~driveFileId" "~permissionId"
+"""
+
+import csv
+import re
+import sys
+
+FILE_NAME = 'name'
+ALT_FILE_NAME = 'title'
+
+QUOTE_CHAR = '"' # Adjust as needed
+LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
+
+PERMISSIONS_N_TYPE = re.compile(r"permissions.(\d+).type")
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+outputCSV = csv.DictWriter(outputFile,
+                           ['Owner', 'driveFileId', 'driveFileTitle', 'permissionId', 'role', 'type', 'allowFileDiscovery', 'domain'],
+                           lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
+outputCSV.writeheader()
+
+if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
+  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  for k, v in iter(row.items()):
+    mg = PERMISSIONS_N_TYPE.match(k)
+    if mg and v in {'anyone', 'domain'}:
+      permissions_N = mg.group(1)
+      allowFileDiscovery = row.get(f'permissions.{permissions_N}.allowFileDiscovery', str(row.get(f'permissions.{permissions_N}.withLink') == 'False'))
+      if allowFileDiscovery == 'True':
+        outputCSV.writerow({'Owner': row['owners.0.emailAddress'],
+                            'driveFileId': row['id'],
+                            'driveFileTitle': row.get(FILE_NAME, row.get(ALT_FILE_NAME, 'Unknown')),
+                            'permissionId': f'id:{row[f"permissions.{permissions_N}.id"]}',
+                            'role': row[f'permissions.{permissions_N}.role'],
+                            'type': row[f'permissions.{permissions_N}.type'],
+                            'domain': row.get(f'permissions.{permissions_N}.domain', ''),
+                            'allowFileDiscovery': allowFileDiscovery})
+
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
