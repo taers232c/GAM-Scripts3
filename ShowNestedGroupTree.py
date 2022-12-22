d5ca3406b194abf47e3db86592b4bc91d25e8b7a
Ross Scroggs <ross.scroggs@gmail.com>
2022-12-22 07:02:06 -0800

    Update ShowNestedGroupTree.py

--- ShowNestedGroupTree.py
+++ ShowNestedGroupTree.py
@@ -3,7 +3,7 @@
 # Purpose: Produce a file to show hierarchial group membership
 # Note: This requires Advanced GAM:
 #	https://github.com/taers232c/GAMADV-XTD3
-# Customize: SHOW_INDENTED, INDENTATION, DELIMITER
+# Customize: INDENTED_INDENTATION, JSON_INDENTATION, LIST_DELIMITER
 # Python: Use python or python3 below as appropriate to your system; verify that you have version 3
 #  $ python -V   or   python3 -V
 #  Python 3.x.y
@@ -11,24 +11,30 @@
 # 1: Get group members of type group
 #  $ gam redirect csv ./GroupMembers.csv print group-members fields email types group
 # 2: From that list of group members, output a file showing hierarchial group membership
-#  $ python3 ShowNestedGroupTree.py ./GroupMembers.csv ./NextedGroupTree.txt
+#  $ python3 ShowNestedGroupTree.py ./GroupMembers.csv indented|list|json ./NextedGroupTree.txt
 """
 
 import csv
+import json
 import sys
 
 QUOTE_CHAR = '"' # Adjust as needed
 
-SHOW_INDENTED = False
-# True  - Show nesting as indented members
-# False - Show nesting as list
-INDENTATION = 2
-DELIMITER = ','
+INDENTED = 'indented'
+JSON = 'json'
+LIST = 'list'
+
+MODE_CHOICES = [INDENTED, JSON, LIST]
+mode = INDENTED
+
+INDENTED_INDENTATION = 2
+JSON_INDENTATION = 1
+LIST_DELIMITER = ','
 
 def printIndentedGroupTree(email, depth):
   outputFile.write(' '*depth+email+'\n')
   for member in sorted(Groups.get(email, [])):
-    printIndentedGroupTree(member, depth+INDENTATION)
+    printIndentedGroupTree(member, depth+INDENTED_INDENTATION)
 
 def printListGroupTree(email, nestedList):
   nestedList.append(email)
@@ -38,12 +44,26 @@ def printListGroupTree(email, nestedList):
       printListGroupTree(member, nestedList)
       nestedList.pop()
   else:
-    outputFile.write(DELIMITER.join(nestedList)+'\n')
+    outputFile.write(LIST_DELIMITER.join(nestedList)+'\n')
 
-if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
-  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+def printJSONGroupTree(email, jsonList):
+  memberDict = {email: []}
+  memberList = sorted(Groups.get(email, []))
+  if memberList:
+    for member in memberList:
+      printJSONGroupTree(member, memberDict[email])
+  jsonList.append(memberDict)
+
+if (len(sys.argv) > 3) and (sys.argv[3] != '-'):
+  outputFile = open(sys.argv[3], 'w', encoding='utf-8', newline='')
 else:
   outputFile = sys.stdout
+
+if len(sys.argv) > 2:
+  mode = sys.argv[2].lower()
+  if mode not in MODE_CHOICES:
+    sys.stderr.write(f'mode ({mode}) must be {"|".join(MODE_CHOICES)}\n')
+    sys.exit(1)
 
 if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
   inputFile = open(sys.argv[1], 'r', encoding='utf-8')
@@ -56,10 +76,16 @@ for row in inputCSV:
   group = row['group']
   Groups.setdefault(group, [])
   Groups[group].append(row['email'])
-if SHOW_INDENTED:
+if mode == INDENTED:
   for group in sorted(Groups):
     printIndentedGroupTree(group, 0)
-else:
+elif mode == JSON:
+  groupJSONList = []
+  for group in sorted(Groups):
+    printJSONGroupTree(group, groupJSONList)
+  json.dump(groupJSONList, outputFile, indent=JSON_INDENTATION, sort_keys=True)
+  outputFile.write('\n')
+else: # mode == LIST
   for group in sorted(Groups):
     printListGroupTree(group, [])
 
