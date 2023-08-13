aa70fb49719e92357f92c8efdf44d94d315cd5a4
Ross Scroggs <ross.scroggs@gmail.com>
2023-08-13 14:58:00 -0700

    Create CountGroupsByDomain.py

---
+++ CountGroupsByDomain.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+# Purpose: Make a CSV file showing the number of groups per domain in a multi-domain workspace
+# Note: This script can use Basic or Advanced GAM:
+#	https://github.com/GAM-team/GAM
+#	https://github.com/taers232c/GAMADV-XTD3
+# Python: Use python or python3 below as appropriate to your system; verify that you have version 3
+#  $ python -V   or python3 -V
+#  Python 3.x.y
+# Usage:
+# 1: Get groups
+#  $ gam print groups email > ./Groups.csv
+# 2: From that list of groups, output a CSV file with headers "Domain,Groups" that shows the
+#    number of groups per domain
+#  $ python3 CountGroupsByDomain.py ./Groups.csv ./GroupsPerDomain.csv
+"""
+
+import csv
+import sys
+
+QUOTE_CHAR = '"' # Adjust as needed
+LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
+
+if (len(sys.argv) > 2) and (sys.argv[2] != '-'):
+  outputFile = open(sys.argv[2], 'w', encoding='utf-8', newline='')
+else:
+  outputFile = sys.stdout
+outputCSV = csv.DictWriter(outputFile, ['Domain', 'Groups'], lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
+outputCSV.writeheader()
+
+if (len(sys.argv) > 1) and (sys.argv[1] != '-'):
+  inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+else:
+  inputFile = sys.stdin
+
+domainGroupCounts = {}
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
+  email = row.get('email', row.get('Email'))
+  if email:
+    name, domain = email.split('@')
+    domainGroupCounts.setdefault(domain, 0)
+    domainGroupCounts[domain] += 1
+for domain, count in sorted(iter(domainGroupCounts.items())):
+  outputCSV.writerow({'Domain': domain, 'Groups': count})
+
+if inputFile != sys.stdin:
+  inputFile.close()
+if outputFile != sys.stdout:
+  outputFile.close()
