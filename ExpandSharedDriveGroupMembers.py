caea8ff94ee240ca4bf09d3f26380e0f01d7c737
Ross Scroggs <ross.scroggs@gmail.com>
2025-04-02 09:45:16 -0700

    Update ExpandSharedDriveGroupMembers.py

--- ExpandSharedDriveGroupMembers.py
+++ ExpandSharedDriveGroupMembers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-# Purpose: Read a CSV file showing group members and a CSV file showing Shared Drive membership
+# Purpose: Read a CSV file showing Group Membership and a CSV file showing Shared Drive membership
 #          and output a CSV file showing the individual group members as members of the Shared Drives.
 #          The output CSV file has additional columns: email, level, subgroup
 #          email - The email address of the user
@@ -27,27 +27,28 @@ import sys
 QUOTE_CHAR = '"' # Adjust as needed
 LINE_TERMINATOR = '\n' # On Windows, you probably want '\r\n'
 
-# Get Group Membership
-inputFile = open(sys.argv[1], 'r', encoding='utf-8')
-inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
+# Group Membership file
 GroupMembers = {}
-for row in inputCSV:
+inputFile = open(sys.argv[1], 'r', encoding='utf-8')
+for row in csv.DictReader(inputFile, quotechar=QUOTE_CHAR):
   if row['type'] == 'USER':
     group = row['group'].lower()
     GroupMembers.setdefault(group, [])
-    GroupMembers[group].append({'email': row['email'].lower(), 'level': row['level'], 'subgroup': row['subgroup']})
+    GroupMembers[group].append({'email': row['email'], 'level': row['level'], 'subgroup': row['subgroup']})
 inputFile.close()
 
-# Get Shared Drive Mmbership
+# Shared Drive Membership file
 inputFile = open(sys.argv[2], 'r', encoding='utf-8')
 inputCSV = csv.DictReader(inputFile, quotechar=QUOTE_CHAR)
 inputFieldNames = inputCSV.fieldnames
-# Expanded Shared Drive Membership
+
+# Expanded Shared Drive Membership file
 outputFile = open(sys.argv[3], 'w', encoding='utf-8', newline='')
 outputFieldNames = inputFieldNames+['email', 'level', 'subgroup']
 outputCSV = csv.DictWriter(outputFile, outputFieldNames, lineterminator=LINE_TERMINATOR, quotechar=QUOTE_CHAR)
 outputCSV.writeheader()
 
+# Expand Shared Drive group members
 for row in inputCSV:
   email = row['permission.emailAddress'].lower()
   if row['permission.type'] != 'group' or email not in GroupMembers:
