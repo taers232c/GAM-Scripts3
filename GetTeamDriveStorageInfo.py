33196168ce6bd036f37e8f304bbcf0771c1afc62
Ross Scroggs <ross.scroggs@gmail.com>
2026-01-16 07:45:36 -0800

    Update GetTeamDriveStorageInfo.py

--- GetTeamDriveStorageInfo.py
+++ GetTeamDriveStorageInfo.py
@@ -5,25 +5,11 @@
 #  $ python -V   or   python3 -V
 #  Python 3.x.y
 # Usage:
-# 1: If you want to include all Team Drives, do this step and then skip to step 4, otherwise start at step 2.
-#  $ gam redirect csv ./TeamDrives.csv print teamdrives fields id,name
-# 2: If you want Team Drives for a specific set of organizers, replace <UserTypeEntity> with your user selection in the command below
-#  $ gam redirect csv ./AllTeamDrives.csv <UserTypeEntity> print teamdrives role organizer fields id,name
-# 3: Delete duplicate Team Drives (some may have multiple organizers). Make sure that ID_FIELD = 'id' in DeleteDuplicateRows.py
-#  $ python3 DeleteDuplicateRows.py ./AllTeamDrives.csv ./TeamDrives.csv
-# 4: Get ACLs for all Team Drives
-#  $ gam redirect csv ./TeamDriveACLs.csv multiprocess csv ./TeamDrives.csv gam print drivefileacls "~id" fields emailaddress,role,type
-# 5: From that list of ACLs, output a CSV file with headers "id,name,organizers"
-#    that shows the organizers for each Team Drive
-# 6: Customize GetTeamDriveOrganizers.py
-#    Set DOMAIN_LIST as desired
-#    Set ONE_ORGANIZER = True
-#    Set SHOW_GROUP_ORGANIZERS = False
-#    Set SHOW_USER_ORGANIZERS = True
-#  $ python3 GetTeamDriveOrganizers.py TeamDriveACLs.csv TeamDrives.csv TeamDriveOrganizers.csv
-# 7: Get Team Drive files
+# 1: Get a list of Team Drives/organizers
+#  $ gam redirect csv ./TeamDriveOrganizers.csv print shareddriveorganizers includefileorganizers
+# 2: Get Team Drive files
 #  $ gam config csv_input_row_filter "organizers:regex:^.+$" redirect csv ./TeamDriveFileCountsSize.csv multiprocess csv ./TeamDriveOrganizers.csv gam user "~organizers" print filecounts select teamdriveid "~id" showsize
-# 8: Get Team Drive storage info
+# 3: Get Team Drive storage info
 #  $ python3 GetTeamDriveStorageInfo.py TeamDriveFileCountsSize.csv TeamDriveStorageInfo.csv
 
 """
